3efae72d62792bd335533800218cee830a1c243d
rob miller <funky.style@gmail.com>
2026-03-23 14:16:20 +0800

    feat: add pip to azure-cli to allow extension installs

--- azure-cli/hooks/pre.sh
+++ azure-cli/hooks/pre.sh
@@ -15,5 +15,7 @@ fedora() {
 
 
 generic() {
-    uv_install azure-cli
+    # Pip needs to be installed alongside azure-cli in order for
+    # installing argcomplete alongside means we don't have to rely on system packages
+    uv_install azure-cli --with pip,argcomplete
 }
