bc0a3c8ff866d9827059a695e1845716679154a9
rob miller <funky.style@gmail.com>
2026-05-11 15:34:49 +0800

    fix: change uv to use caifs_install

--- uv/hooks/pre.sh
+++ uv/hooks/pre.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 generic() {
+    export UV_INSTALL_DIR=./bin
+    export UV_NO_MODIFY_PATH=1
     curl -LsSf https://astral.sh/uv/install.sh | sh
+
+    rm bin/env*
+    ls -lah bin
+    caifs_install bin
 }
