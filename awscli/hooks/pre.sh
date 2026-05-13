4f3fa522006eac7dd77eca1a78c7fde10138571f
rob miller <funky.style@gmail.com>
2026-05-13 12:48:58 +0800

    fix: update glab and awscli to use caifs_install

--- awscli/hooks/pre.sh
+++ awscli/hooks/pre.sh
@@ -3,8 +3,28 @@
 
 
 linux() {
+    # cli does a lot of absolute symbolic linking, have to replicate this to allow it to be installed locally
+    # and at system level
     curl -s "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip"
     unzip awscliv2.zip
-    ./aws/install --bin-dir $HOME/.local/bin/ --install-dir $HOME/.local/awscli/ --update
-    mkdir -p ~/.aws
+    ./aws/install --bin-dir local/bin --install-dir local/awscli --update
+
+    current_link=$(readlink local/awscli/v2/current)
+    version=$(basename "$current_link")
+    echo "Current version is: $version"
+
+    unlink local/bin/aws
+    unlink local/bin/aws_completer
+    unlink local/awscli/v2/current
+
+    mkdir -p local/lib/
+    mv local/awscli/v2/"$version" local/lib/awscli
+
+    cd local/bin
+    ln -s ../lib/awscli/bin/aws aws
+    ln -s ../lib/awscli/bin/aws_completer aws_completer
+
+    cd ../..
+
+    caifs_install "local/*"
 }
--- glab/hooks/pre.sh
+++ glab/hooks/pre.sh
@@ -9,5 +9,8 @@ linux() {
     FILENAME="glab_${VERSION}_linux_amd64"
 
     curl -Ls https://gitlab.com/gitlab-org/cli/-/releases/v${VERSION}/downloads/${FILENAME}.tar.gz | tar xvzf -
-    mv bin/glab ~/.local/bin/glab
+    mkdir -p local/bin
+    mv bin/glab local/bin/
+    chmod +x local/bin/glab
+    caifs_install local/*
 }
