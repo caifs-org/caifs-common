72ad6a8e003c8af54c46243a00880a6aaf0812de
rob miller <funky.style@gmail.com>
2026-06-06 14:04:29 +0800

    chore: updates for binaries with specific arch requirements

--- just-lsp/hooks/pre.sh
+++ just-lsp/hooks/pre.sh
@@ -1,14 +1,51 @@
-#!/bin/sh
+
+macos() {
+    brew install terror/tap/just-lsp
+}
+
+bazzite() {
+    brew install terror/tap/just-lsp
+}
 
 arch() {
     yay_install just-lsp
 }
 
 linux() {
+
+    MACHINE_TYPE="$(uname -m)"
+    case $MACHINE_TYPE in
+        i386 | i486 | i586 | i686 | i786 | x86)
+            ARCH="386"
+            ;;
+        amd64 | x86_64 | x64)
+            ARCH="x86_64"
+            ;;
+        arm | armv7l)
+            ARCH="arm"
+            ;;
+        aarch64)
+            ARCH="aarch64"
+            ;;
+        s390x)
+            ARCH="s390x"
+            ;;
+        ppc64)
+           ARCH="ppc64"
+           ;;
+        ppc64le)
+           ARCH="ppc64le"
+           ;;
+        *)
+            echo "Unknown machine type: $MACHINE_TYPE"
+            exit 1
+            ;;
+    esac
+
     LATEST_VERSION=$(github_latest_tag "terror/just-lsp")
     VERSION=${JUST_LSP_VERSION:=$LATEST_VERSION}
 
-    FILENAME=just-lsp-${VERSION}-x86_64-unknown-linux-gnu.tar.gz
+    FILENAME="just-lsp-${VERSION}-${ARCH}-unknown-linux-gnu.tar.gz"
     curl -sLO https://github.com/terror/just-lsp/releases/download/${VERSION}/${FILENAME}
     tar -xzf ${FILENAME}
     mv just-lsp "${CAIFS_INSTALL_DIR}"/bin/
--- mqttui/hooks/pre.sh
+++ mqttui/hooks/pre.sh
@@ -1,10 +1,47 @@
+macos() {
+    brew install mqttui
+}
+
+bazzite() {
+    brew install mqttui
+}
+
 linux() {
+
+    MACHINE_TYPE="$(uname -m)"
+    case $MACHINE_TYPE in
+        i386 | i486 | i586 | i686 | i786 | x86)
+            ARCH="386"
+            ;;
+        amd64 | x86_64 | x64)
+            ARCH="x86_64"
+            ;;
+        arm | armv7l)
+            ARCH="arm"
+            ;;
+        aarch64)
+            ARCH="arm64"
+            ;;
+        s390x)
+            ARCH="s390x"
+            ;;
+        ppc64)
+           ARCH="ppc64"
+           ;;
+        ppc64le)
+           ARCH="ppc64le"
+           ;;
+        *)
+            echo "Unknown machine type: $MACHINE_TYPE"
+            exit 1
+            ;;
+    esac
 
     LATEST_VERSION=$(github_latest_tag "EdJoPaTo/mqttui")
     VERSION=${MQTTUI_VERSION:-$LATEST_VERSION}
 
-    FILENAME=mqttui-v${VERSION}-x86_64-unknown-linux-gnu.tar.gz
-    curl -sL https://github.com/EdJoPaTo/mqttui/releases/download/v${VERSION}/$FILENAME | tar -zvxf -
+    FILENAME="mqttui-v${VERSION}-${ARCH}-unknown-linux-gnu.tar.gz"
+    curl -sL "https://github.com/EdJoPaTo/mqttui/releases/download/v${VERSION}/$FILENAME" | tar -zvxf -
 
     chmod 0755 mqttui
     chmod 0755 completions/mqttui.bash
